7dac4523742a38b34180c5e6cd48c8a6947d7d03
crehmann <crehmann@users.noreply.github.com>
2018-08-02 20:59:39 +0200

    bugfix

--- CoPiServer/preview/Preview.js
+++ CoPiServer/preview/Preview.js
@@ -27,7 +27,7 @@ const extractRaw = async (device, filePath) => {
       if (err) {
         reject(err);
       } else {
-        resize(previews[previews.size - 1]);
+        resolve(previews[previews.size - 1]);
       }
     });
   });
