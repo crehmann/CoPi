a855fd30dfbc8b3d3052d22adb1f1f25574fe1d3
crehmann <crehmann@users.noreply.github.com>
2018-08-09 22:04:26 +0200

    fixed demo executor

--- CoPiServer/copyjob/CopyJob.js
+++ CoPiServer/copyjob/CopyJob.js
@@ -1,91 +1,119 @@
 class CopyJob {
-    constructor(id, copyJobOptions, copyJobExecution) {
-        this._id = id;
-        this._copyJobOptions = copyJobOptions;
-        this._copyJobExecution = copyJobExecution;
-    }
+  constructor(id, copyJobOptions, copyJobExecution) {
+    this._id = id;
+    this._copyJobOptions = copyJobOptions;
+    this._copyJobExecution = copyJobExecution;
+  }
 
-    get id() { return this._id; }
-    get copyJobOptions() { return this._copyJobOptions; }
-    get copyJobExecution() { return this._copyJobExecution; }
+  get id() {
+    return this._id;
+  }
+  get copyJobOptions() {
+    return this._copyJobOptions;
+  }
+  get copyJobExecution() {
+    return this._copyJobExecution;
+  }
 
-    toDto() {
-        return {
-            id: this._id,
-            source: this._copyJobOptions.source,
-            destination: this._copyJobOptions.destination,
-            flags: this._copyJobOptions.flags,
-            options: this._copyJobOptions.options,
-            command: this._copyJobExecution.command,
-            progress: this._copyJobExecution.progress,
-            state: this._copyJobExecution.state,
-            output: this._copyJobExecution.output,
-            error: this._copyJobExecution.error
-        };
-    }
+  toDto() {
+    return {
+      id: this._id,
+      source: this._copyJobOptions.source,
+      destination: this._copyJobOptions.destination,
+      flags: this._copyJobOptions.flags,
+      options: this._copyJobOptions.options,
+      command: this._copyJobExecution.command,
+      progress: this._copyJobExecution.progress,
+      state: this._copyJobExecution.state,
+      output: this._copyJobExecution.output,
+      error: this._copyJobExecution.error
+    };
+  }
 }
 
 class CopyJobOptions {
-    constructor(source, destination, flags, options) {
-        this._source = source;
-        this._destination = destination;
-        this._flags = flags;
-        this._options = options;
-    }
+  constructor(source, destination, flags, options) {
+    this._source = source;
+    this._destination = destination;
+    this._flags = flags;
+    this._options = options;
+  }
 
-    get source() { return this._source; }
-    get destination() { return this._destination; }
-    get flags() { return this._flags; }
-    get options() { return this._options; }
+  get source() {
+    return this._source;
+  }
+  get destination() {
+    return this._destination;
+  }
+  get flags() {
+    return this._flags;
+  }
+  get options() {
+    return this._options;
+  }
 }
 
 class CopyJobExecution {
-    constructor(process, command) {
-        this._process = process;
-        this._command = command;
-        this._progress = 0;
-        this._output = [];
-        this._state = "open";
-        this._error = null;
-    }
+  constructor(process, command) {
+    this._process = process;
+    this._command = command;
+    this._progress = 0;
+    this._output = [];
+    this._state = "inProgress";
+    this._error = null;
+  }
 
-    get process() { return this._process; }
-    get command() { return this._command; }
-    get progress() { return this._progress; }
-    get output() { return this._output; }
-    get state() { return this._state; }
-    get error() { return this._error; }
+  get process() {
+    return this._process;
+  }
+  get command() {
+    return this._command;
+  }
+  get progress() {
+    return this._progress;
+  }
+  get output() {
+    return this._output;
+  }
+  get state() {
+    return this._state;
+  }
+  get error() {
+    return this._error;
+  }
 
-    set progress(progress) { this._progress = progress; }
-    set error(error) {
-        this._error = error;
-    }
+  set progress(progress) {
+    this._progress = progress;
+  }
+  set error(error) {
+    this._error = error;
+  }
 
-    setInProgress() {
-        this._state = "inProgress";
-    }
+  setInProgress() {
+    this._state = "inProgress";
+  }
 
-    setCompleted() {
-        this._state = "completed";
-        this._progress = 100;
-    }
+  setCompleted() {
+    this._state = "completed";
+    this._progress = 100;
+  }
 
-    setCanceled() {
-        this._state = "canceled";
-    }
+  setCanceled() {
+    this._state = "canceled";
+  }
 
-    setFailed(error) {
-        this._error = error;
-        this._state = "failed";
-    }
+  setFailed(error) {
+    this._error = error;
+    this._state = "failed";
+  }
 
-    appendOutput(line) {
-        this._output.push(line);
-    }
+  appendOutput(line) {
+    this._output.push(line);
+  }
 }
 
 module.exports = {
-    CopyJob: CopyJob,
-    CopyJobOptions: CopyJobOptions,
-    CopyJobExecution: CopyJobExecution
-}
+  CopyJob: CopyJob,
+  CopyJobOptions: CopyJobOptions,
+  CopyJobExecution: CopyJobExecution
+};
--- CoPiServer/copyjob/DemoExecutor.js
+++ CoPiServer/copyjob/DemoExecutor.js
@@ -1,29 +1,31 @@
-const uuid = require('uuid/v1');
+const uuid = require("uuid/v1");
 
-const { CopyJob, CopyJobExecution } = require('./CopyJob')
-const socketService = require('../utils/SocketService');
+const { CopyJob, CopyJobExecution } = require("./CopyJob");
+const socketService = require("../utils/SocketService");
 
-const execute = (copyJobOptions) => {
-    var counter = 0;
-    const execution = new CopyJobExecution(undefined, "DummyCommand");
-    const copyJob = new CopyJob(uuid(), copyJobOptions, execution)
-    const interval = setInterval(() => {
-        counter++;
+const execute = copyJobOptions => {
+  var counter = 0;
+  const execution = new CopyJobExecution(undefined, "DummyCommand");
+  const copyJob = new CopyJob(uuid(), copyJobOptions, execution);
+  const interval = setInterval(() => {
+    counter++;
 
-        if (counter > 10) {
-            clearInterval(interval);
-            execution.completed = true;
-            return;
-        }
+    if (counter > 10) {
+      clearInterval(interval);
+      execution.setCompleted();
+      return;
+    }
 
-        execution.progress = counter * 10;
-        execution.appendFile("SomeFile_" + counter + ".png");
-        socketService.emit("copyJobProgress", { id: copyJob.id, progress: execution.progress });
+    execution.progress = counter * 10;
+    execution.appendOutput("SomeFile_" + counter + ".png");
+    socketService.emit("copyJobProgress", {
+      id: copyJob.id,
+      progress: execution.progress
+    });
+  }, 1000);
+  return copyJob;
+};
 
-    }, 1000);
-    return copyJob;
-}
-
-const cancel = (copyJob) => { };
+const cancel = copyJob => {};
 
 module.exports = { execute, cancel };
